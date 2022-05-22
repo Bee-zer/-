36e391cf90b011d6c488ab16e4f518d8789b6a83
Alexey <zoom-alex@yandex.ru>
2022-05-22 22:20:00 +0300

    Add homework 2

--- Lesson_2/Lesson_2.1.py
+++ Lesson_2/Lesson_2.1.py
@@ -4,7 +4,8 @@ import requests
 
 base_url = 'https://spb.hh.ru/search/vacancy'
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36'}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36'}
 
 keyword = input('Введите вакансию: ')
 
@@ -89,5 +90,6 @@ data = data_collect(max_page)
 def data_to_json(data):
     with open(f'{keyword}.json', 'w', encoding='utf-8') as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
+
 
 data_to_json(data)
