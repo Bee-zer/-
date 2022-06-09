24d170c6706b8d9203003ea43ace64d28a264c34
Alexey <zoom-alex@yandex.ru>
2022-06-09 20:08:46 +0300

    Add HW 7

--- castorama/middlewares.py
+++ castorama/middlewares.py
@@ -9,7 +9,7 @@ from scrapy import signals
 from itemadapter import is_item, ItemAdapter
 
 
-class MarketparserSpiderMiddleware:
+class CastoramaSpiderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the spider middleware does not modify the
     # passed objects.
@@ -57,7 +57,7 @@ class MarketparserSpiderMiddleware:
         spider.logger.info('Spider opened: %s' % spider.name)
 
 
-class MarketparserDownloaderMiddleware:
+class CastoramaDownloaderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the downloader middleware does not modify the
     # passed objects.
